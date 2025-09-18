16f87310e54c3af4513c7b222fcfc3ea55325284
Alexis Verne <a.verne@ensi.lan>
2025-09-18 17:03:56 +0200

    Other exercices

---
+++ Assets/GameLoopEtHiérarchie/Ex1.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace GameLoopEtHiérarchie;
+public class EX1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
